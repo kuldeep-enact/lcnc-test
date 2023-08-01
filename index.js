929d251dbacdec7aebbc9201d81b46ad368ab19e
kuldeep-enact <139554260+kuldeep-enact@users.noreply.github.com>
2023-08-01 13:33:14 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = 8080
+const port = 9900 || process.env.PORT
 
 app.get('/', (req, res) => {
   res.send('Hello World from test app !')
